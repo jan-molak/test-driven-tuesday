28b781a6545e92f3f2a4d82c0ca71a890bb71fd9
jan-molak <jan.molak@gmail.com>
2014-06-03 08:59:50 +0100

    Corrected the LittleMocker::Authoriser so that it works correctly with Sinon's createStubInstance

--- src/little_mocker/authoriser.js
+++ src/little_mocker/authoriser.js
@@ -3,22 +3,19 @@
 /**
  * @constructor
  */
-function Authoriser() {
+function Authoriser() { }
 
-    return {
-        /**
-         * @param {String} username
-         * @param {String} password
-         *
-         * @returns {Boolean}
-         */
-        authorise: function(username, password) {
-            console.info('Authoriser#authorise calls an external system ...');
+/**
+ * @param {String} username
+ * @param {String} password
+ *
+ * @returns {Boolean}
+ */
+Authoriser.prototype.authorise = function(username, password) {
+    console.info('Authoriser#authorise calls an external system ...');
 
-            // returns true or false randomly, so we're not tempted to rely on its behaviour ;-)
-            return (Math.random() < 0.5);
-        }
-    };
-}
+    // returns true or false randomly, so we're not tempted to rely on its behaviour ;-)
+    return (Math.random() < 0.5);
+};
 
 module.exports = Authoriser;
